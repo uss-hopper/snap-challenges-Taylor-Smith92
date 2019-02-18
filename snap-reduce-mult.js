83cb240de77185df60f94cc8002e9c0c38ff72db
Taylor Smith <taylorleesmith92@gmail.com>
2019-02-18 08:42:46 -0700

    added missing 21 in array

--- snap-reduce-mult.js
+++ snap-reduce-mult.js
@@ -1,11 +1,11 @@
-let array = [1,2,3,5,8,13,34];
+let array = [1,2,3,5,8,13,21,34];
 
 window.onload = () => {
 	const multiplyNums = array.reduce(
 		(accumulator, index) => {
 			return accumulator * index;
 		}
-	)
+	);
 	let target = document.getElementById("target");
 	target.innerHTML = multiplyNums;
-}
+};

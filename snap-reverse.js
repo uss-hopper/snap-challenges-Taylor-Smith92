f40c29acb41e9ffe8c1fffc3e1e6e93efbe07148
Taylor Smith <taylorleesmith92@gmail.com>
2019-02-21 09:07:09 -0700

    added George's solution

--- snap-reverse.js
+++ snap-reverse.js
@@ -1,17 +1,22 @@
 let string = "I'll alert the crew. We finished our first sensor sweep of the neutral zone. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody.";
 
-let words = string.split(" ");
+let words = string.split("");
 /** backW = function backwards() {
 *	 return words.reverse();
 * }
 * console.log(backW());
 **/
 
-function backwards(accumulator, currentValue) {
+/**
+ * function backwards(accumulator, currentValue) {
 	let combine = words.reverse();
 	return (combine.join(" "))
 }
+ let  final = words.reduce(backwards, 0);
+ **/
 
-const final = words.reduce(backwards, 0);
+
+let  final = words.reduce((accumulator, current) => current + " " + accumulator);
 
 console.log(final);
+

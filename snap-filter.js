652674bd3ae83131cfac47a4bcb0927d7d2ddb75
Taylor Smith <taylorleesmith92@gmail.com>
2019-03-05 09:34:07 -0700

    updates from george

--- snap-filter.js
+++ snap-filter.js
@@ -2,10 +2,11 @@
 
 let string = "Ensign Babyface! Your shields were failing, sir. This should be interesting. Your head is not an artifact! What's a knock-out like you doing in a computer-generated gin joint like this? Why don't we just give everybody a promotion and call it a night - 'Commander'? But the probability of making a six is no greater than that of rolling a seven. What? We're not at all alike! I recommend you don't fire until you're within 40,000 kilometers. Wouldn't that bring about chaos? Flair is what marks the difference between artistry and mere competence.";
 
-let array = string.split(" ");
+let array = string.replace(/[!.,?-]+/g,"").split(" ");
+console.log(array);
 
 filterArray = array.filter(index => {
-	return index[0].toLowerCase() !== "e";
+	return index.charAt(0).toLowerCase() !== "e";
 });
 
 noEs = filterArray.join(" ");
